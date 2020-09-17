4cd14fb42f19163fb134215dbb9dc5f6157ae6f9
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-09-17 10:46:29 -0400

    fixed backend routes and tested on postman

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -43,7 +43,8 @@ class GamesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = @current_user.games.find(params[:id])
+      @game = @current_user.game.find(params[:id])
+  
     end
 
     # Only allow a trusted parameter "white list" through.
