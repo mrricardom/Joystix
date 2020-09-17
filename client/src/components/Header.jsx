d7f5748625269c0e86baf04342461a65733ca225
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-09-17 16:07:52 -0400

    added header, added header to layout, added auth functions to app

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Joystick from '../images/Joystick.png'
+import '../css/Header.css'
 
-export default function Header() {
-  return <div></div>
+export default function Header(props) {
+  const { currentUser } = props
+  return (
+    <header>
+      <Link to='/'>
+        <h1 className='site-logo'> Joystix</h1>
+      </Link>
+      <img src={Joystick} />
+      {currentUser ? (
+        <div className='login-register-option'>
+          <p>{currentUser.username}</p>
+          <button onClick={props.handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <Link to='/login' className='login-register-option'>
+          Log-In/Register
+        </Link>
+      )}
+    </header>
+  )
 }
