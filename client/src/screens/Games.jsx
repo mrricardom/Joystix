71c46664de9410bc2cf73e787001ff48c365f313
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-09-17 21:20:20 -0400

    added Games.jsx

--- client/src/screens/Games.jsx
+++ client/src/screens/Games.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Games() {
-  return <div></div>
+export default function Games(props) {
+  const { currentUser, games, handleDelete } = props
+  return (
+    <div>
+      <p>Hi</p>
+    </div>
+  )
 }
