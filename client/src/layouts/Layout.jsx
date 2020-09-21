babb73b5060442503fbf8d37d718d52d443afa78
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-09-21 17:23:14 -0400

    buttons on top are fixed

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../css/Layout.css'
 
 export default function Layout(props) {
+  const location = useLocation()
+  console.log(location)
+
+  const loginCheck = () => {
+    if (location.pathname !== '/login' && location.pathname !== '/register') {
+      return (
+        <div className='top-buttons'>
+          <Link to='/'>
+            <button className='arcade-button'>Arcade</button>
+          </Link>
+          <Link to='/mygames'>
+            <button className='mygames-button'>My Games</button>
+          </Link>
+        </div>
+      )
+    }
+  }
+
   return (
     <div className='app-body'>
       <Header
@@ -12,12 +30,7 @@ export default function Layout(props) {
         handleLogout={props.handleLogout}
       />
       <main>
-        <Link to='/'>
-          <button className='arcade-button'>Arcade</button>
-        </Link>
-        <Link to='/mygames'>
-          <button className='mygames-button'>My Games</button>
-        </Link>
+        {loginCheck()}
         {props.children}
         <Footer />
       </main>
